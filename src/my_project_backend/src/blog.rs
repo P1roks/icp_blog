e9d0d87deb57756737e4a55677fbb25e28017d82
Piotr Jakóbczyk <piotrekjakobczyk1@gmail.com>
2024-12-03 16:45:17 +0000

    rework time to use function

--- src/my_project_backend/src/blog.rs
+++ src/my_project_backend/src/blog.rs
@@ -1,15 +1,16 @@
 use candid::CandidType;
+use ic_cdk::api::time;
 
 #[derive(Clone, CandidType)]
 pub struct Blog {
     title: String,
-    date: u32,
+    date: u64,
     content: String,
     tags: Vec<String>,
 }
 
 impl Blog {
-    pub fn new(title: String, date: u32, content: String, tags: Vec<String>) -> Self {
-        Self { title, date, content, tags }
+    pub fn new(title: String, content: String, tags: Vec<String>) -> Self {
+        Self { title, date: time(), content, tags }
     }
 }
