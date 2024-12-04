f331e990e5614e5bb91bb7587b450f18617828e3
Piotr Jakóbczyk <piotrekjakobczyk1@gmail.com>
2024-12-04 17:25:57 +0000

    tag testing

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,23 +1,36 @@
 use std::cell::RefCell;
 
+use crate::config::Config;
 use crate::blog::Blog;
 
 mod blog;
+mod config;
 
 thread_local! {
     static BLOGS: RefCell<Vec<Blog>> = RefCell::new(Vec::new());
+    static CONFIG: RefCell<Config> = RefCell::new(Config::new());
+}
+
+#[ic_cdk::update]
+fn edit_config(new_config: Config) {
+    CONFIG.with(|config| *config.borrow_mut() = new_config);
 }
 
 #[ic_cdk::update]
 fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, String> {
-    if title.len() > 250 {
+    let config = CONFIG.with(|config| config.borrow().clone());
+
+    if title.len() > config.max_title_len as usize {
         Err(String::from("Title is too long"))
     }
-    else if content.len() > 2000 {
+    else if content.len() > config.max_content_len as usize {
         Err(String::from("Content is too long"))
     }
-    else if tags.len() > 3 {
+    else if tags.len() > config.max_tags_count as usize {
         Err(String::from("Too many tags"))
+    }
+    else if tags.iter().any(|tag| !config.tags.contains(tag)) {
+        Err(String::from("Tag doesn't exist"))
     }
     else {
         BLOGS.with(|blogs| {
